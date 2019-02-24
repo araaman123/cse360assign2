ed49a68d8138fc2b951ae7f7a330399bd1e505c9
Ankith <ankithraaman@gmail.com>
2019-02-24 10:57:47 -0700

    Initial Version

---
+++ cse360assign2/src/cse360assign2/Calculator.java
@@ -0,0 +1,51 @@
+/*
+Ankith Raaman
+
+Class ID: 529
+
+Assignment # 2
+
+Using version control systems
+ */
+package cse360assign2;
+
+
+public class Calculator {
+
+	private int total;
+	
+	public Calculator () 
+	{
+		total = 0;  // not needed - included for clarity
+	}
+	
+	public int getTotal () //method to access total
+	{
+		return 0;
+	}
+	
+	public void add (int value) //add function
+	{
+		
+	}
+	
+	public void subtract (int value) //subtract function
+	{
+		
+	}
+	
+	public void multiply (int value) //multiply function
+	{
+		
+	}
+	
+	public void divide (int value) //divide function
+	{
+		
+	}
+	
+	public String getHistory () //function to get history of actions
+	{
+		return "";
+	}
+}
